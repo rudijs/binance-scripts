fcb20bc733ef7426a3361cf141aa66cf7cd7b34d
Rudi Starcevic <rudi.starcevic@viseo.com>
2018-08-02 22:05:17 +0800

    Account order detail formatting

--- account.js
+++ account.js
@@ -5,18 +5,26 @@ const lib = require("./lib");
 
 (async function() {
   try {
-    console.log(`\n==> ACCOUNT INFO <==\n`);
+    console.log(`\n==> ACCOUNT INFO <==`);
 
     const accountInfo = await lib.accountInfo(client);
 
-    console.log("==> Balances:");
+    console.log("\n==> Balances:");
     console.log(accountInfo.balances);
 
-    console.log("==> Open Orders:");
+    console.log("\n==> Open Orders:");
     console.log(accountInfo.openOrders);
 
-    console.log("==> Open Orders Details:");
-    console.log(accountInfo.openOrderDetails);
+    console.log("\n==> Open Orders Details:");
+    // console.log(accountInfo.openOrderDetails);
+    accountInfo.openOrderDetails.forEach(item => {
+      console.log("");
+      console.log("Symbol:", item.symbol, "-", item.side);
+      console.log("Stop Price:".padEnd(14), item.stopPrice);
+      console.log("Price:".padEnd(14), item.price);
+      console.log("Orig QTY:".padEnd(14), item.origQty);
+      console.log("Executed QTY:".padEnd(14), item.executedQty);
+    });
   } catch (e) {
     console.log("==> Error");
     console.log(e);
